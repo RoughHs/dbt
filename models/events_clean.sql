1c4c816e76564e137ccb97bf33d13801aba1e11f
RoughHs <mikea_av@mail.ru>
2025-01-09 10:54:36 +0300

    Events

---
+++ models/events_clean.sql
@@ -0,0 +1,10 @@
+select distinct user_id
+      ,"timestamp"
+      ,type_id
+from {{ source('scooters_raw','events') }}
+where
+{% if is_incremental() %}
+    "timestamp" > (select max("timestamp") from {{ this }})
+{% else %}
+    "timestamp" < '2023-08-01'
+{% endif %}
